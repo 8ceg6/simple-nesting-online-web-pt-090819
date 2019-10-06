6a8df98b6bbf2633f8a51416aaa985921b8008a4
cedric e goddard <8ceg6@comcast.net>
2019-10-06 21:55:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,12 +85,13 @@ programmer_hash =
           :known_for => "Unix",
           :languages => ["C"]
         }
-     
+ 		}
 
   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-}
+
 
 end
+
 
 
 def changing_alan
